2eba00d27493a54f93c09d9f3fbe0a7f896431a7
ayoub3bidi <mrayoubabidi@gmail.com>
2021-12-26 11:39:05 +0100

    ✅ Setting up the requirements

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const multer = require("multer");
+const {TesseractWorker} = require("tesseract.js");
+const worker = new TesseractWorker();
+
+const storage = multer.diskStorage({
+    destination: (req,res,cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req,res,cb) => {
+        cb(null, req.file);
+    }
+});
+
+const upload = multer({storage: storage}).single("avatar");
+app.set("view engine", "ejs");
